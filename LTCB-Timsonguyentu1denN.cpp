ee262a425e1b1877f363c7d5ffd408e9066290ac
SPC <SPC@25-LAB402>
2024-07-01 20:02:52 +0700

    Add project files.

---
+++ LTCB-Timsonguyentu1denN.cpp
@@ -0,0 +1,33 @@
+// LTCB-Timsonguyentu1denN.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+bool checkSNT(int so);
+int main()
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (checkSNT(i)) cout << i << " ";
+	}
+}
+bool checkSNT(int so)
+{
+	int dem = 0
+		for (int i = 1; i <= so; i++)
+		{
+			if (so % i == 0) dem++;
+		}
+	if (dem == 2) return true;
+	return false;
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
